47148215f070f7cc74436714475e4d477bdd3d17
ahmadzoubi99 <ahmadalzoubi099@gmail.com>
2024-06-26 14:17:49 +0300

    aa

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task2
+{
+    internal class Program
+    {
+        public void task5(int num1, int num2, int num3, int num4, int num5)
+        {
+            int max = num1;
+            if (num2 > max)
+            {
+                max = num2;
+            }
+            if (num3 > max)
+            {
+                max = num3;
+            }
+            if (num4 > max)
+            {
+                max = num4;
+            }
+            if (num5 > max)
+            {
+                max = num5;
+            }
+
+
+
+        }
+        
+        public  double task6(double kilometers)
+        {
+            int miles= kilometers*
+            return
+        }
+        static void Main(string[] args)
+        {
+           //task1
+            int  num1=Convert.ToInt32(Console.ReadLine());
+            int num2=Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2) { Console.WriteLine($" num2 is smaller than num1"); }
+            else if (num1 < num2) { Console.WriteLine($"num1 is smaller than num2"); }
+            else { Console.WriteLine("num1 is equal num2"); }
+            //----------------
+            //task2
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num3 > 0) { Console.WriteLine(" The sign is + "); }
+            if (num3 < 0) { Console.WriteLine(" The sign is - "); }
+            if (num3 == 0) { Console.WriteLine(" The sign is = "); }
+            
+
+            //task3
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+            int num6 = Convert.ToInt32(Console.ReadLine());
+            if (num4 < num5)
+            {
+                if (num4 > num6)
+                {
+                    Console.WriteLine($"{num5} {num4} {num6}");
+                }
+                else
+                {
+                    Console.WriteLine($"{num5} {num6} {num4}");
+
+                }
+            }
+            if (num5 < num4)
+            {
+                if (num5 > num6)
+                {
+                    Console.WriteLine($"{num4} {num5} {num6}");
+                }
+                else
+                {
+                    Console.WriteLine($"{num4} {num6} {num5}");
+
+                }
+            }
+            if (num6 < num4)
+            {
+                if (num5 > num6)
+                {
+                    Console.WriteLine($"{num6} {num5} {num6}");
+                }
+                else
+                {
+                    Console.WriteLine($"{num6} {num6} {num5}");
+
+                }
+            }
+
+            //task 5
+           
+
+        }
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+                string str = Console.ReadLine();
+                Console.WriteLine(str);
+            //-----------------------
+
+            //task2
+            double salary = 300002.2;
+          
+            string str1 = "ahmad";
+            char chr= 'A';
+            int num1 = 1;
+
+            const double pi=3.14;
+            Console.WriteLine(num1);
+            Console.WriteLine(salary);
+            Console.WriteLine(chr);
+            Console.WriteLine(num1);
+            Console.WriteLine(pi);
+
+            //task3
+            string[] car = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+            for (int i = 0; i < car.Length; i++)
+            {
+                Console.WriteLine(car[i]);
+            }
+            //---------------------------------------------
+            //task4
+            Console.Write("First Name :");
+            string firstName=Console.ReadLine();
+            Console.Write("Last Name :");
+            string lastName=Console.ReadLine();
+            Console.Write("Enter Number of year :");
+            int year=Convert.ToInt32(Console.ReadLine());
+            //------------------------------------------------
+            //task5
+
+            int[] array = new int[10];
+            Console.WriteLine("Input 10 elements in the array:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"element - {i} : ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Elements in array are:");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+           Console.WriteLine();
+            //task 6
+            int[] array1=new int[4];
+            int sum = 0;
+            Console.WriteLine("Input 4 elements in the array:");
+            for(int i = 0;i < array1.Length;i++)
+            {
+                array1[i]=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"element - {i} : {array1[i]} ");
+                sum += array1[i];
+            }
+            Console.WriteLine($" Sum of all elements stored in the array is :{sum}");
+
+
+
+        }
+
+    }
+}
