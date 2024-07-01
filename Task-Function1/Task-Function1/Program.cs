adeedba90cefc0fde0bc2ec245f91dbf783a7dc5
ahmadzoubi99 <ahmadalzoubi099@gmail.com>
2024-07-01 22:51:08 +0300

    submit task

---
+++ Task-Function1/Task-Function1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Data;
+
+namespace Task_Function1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //task1
+            void MinutesToSeconds(int minutes)
+            {
+                Console.WriteLine(minutes * 60);
+            }
+            MinutesToSeconds(30);
+
+            //task2
+            int increments(int number)
+            {
+                return ++number;
+            }
+
+            //task3
+            int FirstIndex(int[] number)
+            {
+                return number[0];
+            }
+
+            //task4
+            double AreaTriangle(int baseTriangle, int height)
+            {
+
+                return (.5 * baseTriangle * height);
+            }
+
+            //task5
+            int[] evenNumberEvenIndex(int[] number)
+            {
+                int count = 0;
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[i] % 2 == 0 && i % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                int[] evenArray = new int[count];
+                count = 0;
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[i] % 2 == 0 && i % 2 == 0)
+                    {
+                        evenArray[count++] = number[i];
+                    }
+                }
+                return evenArray;
+            }
+
+            int[] nums = { 5, 2, 2, 1, 8, 66, 55, 77, 34, 9, 55, 1 };
+            foreach (int i in evenNumberEvenIndex(nums))
+            {
+                Console.WriteLine(i);
+            }
+
+            //task6
+            string[] evenIndexOddLength(string[] array)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].Length % 2 != 0 && i % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                string[] oddLength = new string[count];
+                count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].Length % 2 != 0 && i % 2 == 0)
+                    {
+                        oddLength[count++] = array[i];
+                    }
+                }
+                return oddLength;
+            }
+            string[] array = {"alex", "mercer", "madrasa", "rashed2", "emad", "hala"};
+            foreach (string i in evenIndexOddLength(array))
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //task7
+            double[] PowerOfIndex(double[] nums)
+            {
+                for(int i = 0;i < nums.Length;i++)
+                {
+                    nums[i] = Math.Pow(nums[i],i);
+                }
+                return nums;
+            }
+            double[] nums2 = { 44, 5, 4, 3, 2, 10 };
+            foreach (int i in PowerOfIndex(nums2))
+            {
+                Console.Write($" {i}   ");
+            }
+            Console.WriteLine();    
+            //task8
+            int multiplication(int num1,int num2)
+            {
+                return (num1 * num2) ;
+            }
+            
+           Console.WriteLine(multiplication(5, 4));
+
+            //task9
+            int muti2(int num1,int num2)
+            {
+                int num = 1;
+                for (int i = num1; i <=num2; i++)
+                {
+                    num *= i;
+                }
+                return num;
+            }
+            Console.WriteLine(muti2(3, 6));
+
+            //task10
+            double AVG(int[] nums)
+            {
+                double avg = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    avg += nums[i];
+                }
+                return avg/nums.Length;
+            }
+            int[] numaray1={1,4,3,8,9};
+           Console.WriteLine(AVG(numaray1));
+        }
+    }
+}
---
+++ Task-Function2/Task-function2/Task-function2/Program.cs
@@ -0,0 +1,98 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Linq;
+
+namespace Task_Function2
+{
+    internal class Program
+    {
+        public static int GetDays(DateTime firstDate, DateTime secondDate)
+        {
+            TimeSpan difference = secondDate - firstDate;
+            return (int)difference.TotalDays;
+        }
+       
+        static void Main(string[] args)
+        {
+            //task1
+            DateTime date1 = new DateTime(2019, 6, 14);
+            DateTime date2 = new DateTime(2019, 6, 20);
+
+            int daysBetween = GetDays(date1, date2);
+            Console.WriteLine("Number of days between {0} and {1}: {2}", date1.ToShortDateString(), date2.ToShortDateString(), daysBetween);
+
+            //task2
+            string[] test1 = { "1a", "a", "2b", "b" };
+            string[] test2 = { "abc", "abc10" };
+            string[] test3 = { "no", "numbers", "here" };
+
+
+            string[] NumInStr(string[] array)
+            {
+                List<string> result = new List<string>();
+                foreach (string str in array)
+                {
+                    if (str.Any(char.IsDigit)){
+                        result.Add(str);
+                    }
+                }
+                return result.ToArray();
+            }
+            string[] result1 = NumInStr(test1);
+            foreach (string str in result1)
+            {
+                Console.Write($"{str} ");
+            }
+            Console.WriteLine();
+            //task3
+            void reverseOddLength(string statment)
+            {
+
+                List<string> result=new List<string>();
+                string[] array = statment.Split(' ');
+                string str = "";
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].Length % 2 != 0)
+                    {
+                        for (int j = array[i].Length - 1; j >= 0; j--)
+                        {
+                            str += array[i][j];
+                        }
+                        result.Add((string)str);
+                        str = "";
+                    }
+
+                }
+                foreach (string str1 in result.ToArray())
+                {
+                    Console.WriteLine(str1);
+                }
+            }
+            reverseOddLength("One two three four");
+
+            //task4
+            bool pandigitalNumber(string str)
+            {
+                for (int i = 0; i <= 9; i++)
+                {
+                    bool found = false;
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (str[j] == (char)(i + '0'))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            Console.WriteLine(pandigitalNumber("98140723568910"));
+        }
+    }
+}
